bbd80ead0f5f173d6b58208328c513af6a5fea36
Denis Szczukocki <Denis_Szczukocki@epam.com>
2018-08-20 12:37:05 +0200

    add Location http header on job offer creation

--- src/main/java/pl/desz/controller/JobController.java
+++ src/main/java/pl/desz/controller/JobController.java
@@ -12,13 +12,19 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 import java.util.List;
 
 @Path("/jobs")
 @Produces(value = {MediaType.APPLICATION_JSON})
 public class JobController {
+
+    @Context
+    private UriInfo uriInfo;
 
     @Inject
     private JobService jobService;
@@ -39,8 +45,7 @@ public class JobController {
     public Response addOffer(JobOffer newOffer) {
         final JobOffer saved = jobService.add(newOffer);
 
-        return Response.status(Response.Status.CREATED)
-                .entity(saved)
+        return Response.created(URI.create(uriInfo.getRequestUri() + saved.getId()))
                 .type(MediaType.APPLICATION_JSON_TYPE)
                 .build();
     }
