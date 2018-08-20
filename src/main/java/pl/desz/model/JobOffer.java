54a5bd1f3d450870af54ef3f7fa80e67e782464e
Denis Szczukocki <Denis_Szczukocki@epam.com>
2018-08-20 11:14:31 +0200

    add formatting to LocalDateTime, extract props in pom.xml

--- src/main/java/pl/desz/model/JobOffer.java
+++ src/main/java/pl/desz/model/JobOffer.java
@@ -1,5 +1,10 @@
 package pl.desz.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -12,7 +17,12 @@ public class JobOffer {
     private JobOfferOwner contact;
     private String industry;
     private boolean isArchived = false;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime postedDateTime = LocalDateTime.now();
+    
     private long applicationsCount = 0L;
     private JobDetails details;
 
